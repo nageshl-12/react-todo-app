2f98ac67397c45ca3d16fc73f4c29a10efec0a1f
Nagesh l <nageshl15260@gmail.com>
2026-07-05 08:51:18 +0530

    Updated UI and Edited Some JSx

--- src/Components/InputField.jsx
+++ src/Components/InputField.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { ListPlus, BrushCleaning } from "lucide-react";
 
 const InputField = ({ input, setInput, onAdd, onClear }) => {
   const InputEl = useRef(null);
@@ -22,49 +23,16 @@ const InputField = ({ input, setInput, onAdd, onClear }) => {
 
       <button
         onClick={onAdd}
-        className="bg-white flex flex-row gap-2 h-12 border w-fit border-[rgba(0,0,0,0.2)] focus:outline-blue-300 p-2 rounded-2xl text-black cursor-pointer transition duration-300 hover:-translate-y-0.5 hover:shadow-[0_12px_35px_rgba(0,0,255,0.4)]"
+        className="bg-white flex flex-row gap-2 border w-fit border-[rgba(0,0,0,0.2)] focus:outline-blue-300 p-2 rounded-2xl text-black cursor-pointer transition duration-300 hover:-translate-y-0.5 hover:shadow-[0_12px_35px_rgba(0,0,255,0.4)]"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          className="lucide lucide-list-plus-icon lucide-list-plus"
-        >
-          <path d="M16 5H3" />
-          <path d="M11 12H3" />
-          <path d="M16 19H3" />
-          <path d="M18 9v6" />
-          <path d="M21 12h-6" />
-        </svg>
+        <ListPlus size={24} />
         Add
       </button>
       <button
         className="bg-white border w-fit flex gap-1 border-[rgba(0,0,0,0.2)] focus:outline-blue-300 p-2 rounded-2xl text-black cursor-pointer transition duration-300 hover:-translate-y-0.5 hover:shadow-[0_12px_35px_rgba(0,0,255,0.4)]"
         onClick={onClear}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          className="shrink-0"
-        >
-          <path d="m16 22-1-4" />
-          <path d="M19 14a1 1 0 0 0 1-1v-1a2 2 0 0 0-2-2h-3a1 1 0 0 1-1-1V4a2 2 0 0 0-4 0v5a1 1 0 0 1-1 1H6a2 2 0 0 0-2 2v1a1 1 0 0 0 1 1" />
-          <path d="M19 14H5l-1.973 6.767A1 1 0 0 0 4 22h16a1 1 0 0 0 .973-1.233z" />
-          <path d="m8 22 1-4" />
-        </svg>
+        <BrushCleaning size={24} />
         Clear
       </button>
     </div>
--- src/Components/OutputField.jsx
+++ src/Components/OutputField.jsx
@@ -1,3 +1,5 @@
+import { SquarePen, Trash, SaveCheck, CircleCheckBig } from "lucide-react";
+
 const OutputField = ({ list, onDelete, onCheck, onEdit, onValueChange }) => {
   return (
     <div className="bg-yellow-50 h-screen w-full rounded-2xl p-6 border border-[rgba(0,0,0,0.5)] hover:scrollbar-thumb-red-400 overflow-y-auto scrollbar-thumb-red-400 scrollbar-track-blue-200">
@@ -39,78 +41,30 @@ const OutputField = ({ list, onDelete, onCheck, onEdit, onValueChange }) => {
                 <span onDoubleClick={() => onEdit(list.id)}>{list.text}</span>
               )}
 
-              <svg
+              <Trash
+                size={21}
                 onClick={() => onDelete(list.id)}
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                name="deletIcon"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
                 className="shrink-0 cursor-pointer hover:text-red-500 transition-colors "
-              >
-                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
-                <path d="M3 6h18" />
-                <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-              </svg>
+              />
 
               {list.edit ? (
-                <svg
-                  onClick={() => onEdit(list.id)}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="hover:text-blue-500 transition-colors shrink-0 cursor-pointer"
-                >
-                  <path d="M12.5 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h10.2a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4v4.35" />
-                  <path d="m16 19 2 2 4-4" />
-                  <path d="M17 15.13V14a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7" />
-                  <path d="M7 3v4a1 1 0 0 0 1 1h7" />
-                </svg>
-              ) : (
-                <svg
-                  onClick={() => onEdit(list.id)}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                <SaveCheck
+                  size={21}
                   className="cursor-pointer shrink-0 hover:text-blue-500 transition-colors "
-                >
-                  <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-                  <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z" />
-                </svg>
+                  onClick={() => onEdit(list.id)}
+                />
+              ) : (
+                <SquarePen
+                  size={21}
+                  onClick={() => onEdit(list.id)}
+                  className="hover:text-blue-500 transition-colors shrink-0 cursor-pointer"
+                />
               )}
-              <svg
-                onClick={() => onCheck(list.id)}
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="20"
-                viewBox="0 0 24 24"
-                fill={list.checked ? "#13d165" : "none"}
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+              <CircleCheckBig
                 className="shrink-0 cursor-pointer hover:text-green-500 transition-colors "
-              >
-                <path d="M21.801 10A10 10 0 1 1 17 3.335" />
-                <path d="m9 11 3 3L22 4" />
-              </svg>
+                size={21}
+                onClick={() => onCheck(list.id)}
+              />
             </li>
           </div>
         ))}
